adddd760ac563688a666a9286a16fea836f621fc
Mark <markteams@gmail.com>
2018-05-09 15:37:35 +0300

    All the processors are moved into Item class

--- gplay/items.py
+++ gplay/items.py
@@ -1,26 +1,53 @@
 # -*- coding: utf-8 -*-
 
 from scrapy.item import Item, Field
+from scrapy.loader.processors import Join, MapCompose, TakeFirst, Compose
+from w3lib.html import remove_tags
+import re
+
+""" Calculating promotions in app
+    0: no promotion
+    1: Contains Adds
+    2: Offers in-app purchases
+    3: Contains Adds . Offers in-app purchases
+"""
+def promotion_status(value):
+    promotion = 0
+    if ("Contains Ads" in value):
+            promotion = promotion + 1
+    if ("Offers in-app purchases" in value):
+        promotion = promotion + 2
+    return promotion
 
 class GplayItem(Item):
+    
     # Primary fields
-    App_name          = Field()
-    Price             = Field()
-    Editor_choice     = Field()
-    Developer         = Field()
-    Developer_location= Field()
-    Developer_URL     = Field()
-    Genre             = Field()
-    Content_rating    = Field()
-    Promotion         = Field()
-    Description       = Field()
-    App_rate          = Field()
+    App_name          = Field(input_processor=MapCompose(str.strip, str.lower))
+    Genre             = Field(input_processor=MapCompose(str.strip, str.lower),
+                              output_processor=Join("|"))
+    Price             = Field(input_processor=MapCompose(lambda x: re.search(r"[,.0-9]+",x).group()))
+    Editor_choice     = Field(input_processor=Compose(lambda x: "0" if len(x) == 0 else "1"))
+    Developer         = Field(input_processor=MapCompose(str.strip))
+    Developer_URL     = Field(input_processor=MapCompose(str.strip))
+    Content_rating    = Field(input_processor=MapCompose(str.strip))
+    Promotion         = Field(input_processor=Compose(promotion_status))
+    Description       = Field(input_processor=MapCompose(str.strip, 
+                                                         lambda i: i.replace("'", ''), 
+                                                         str.lower,
+                                                         remove_tags),
+                              output_processor=Join())
+    App_rate          = Field(input_processor=MapCompose(float))
     Reviewers_count   = Field()
     Rate_5star        = Field()
     Rate_4star        = Field()
     Rate_3star        = Field()
     Rate_2star        = Field()
     Rate_1star        = Field()
+    Developer_location= Field(input_processor=MapCompose(str.strip, 
+                                                         lambda i: i.replace("'", ''), 
+                                                         lambda i: i.replace("\n", ' '), 
+                                                         str.lower,
+                                                         remove_tags))
     Update_date       = Field()
     Version           = Field()
     Filesize          = Field()
